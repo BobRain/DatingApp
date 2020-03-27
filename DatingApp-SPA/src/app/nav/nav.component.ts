7cac64f449b0906fa1db8ad607174185a3defada
Boris Kashirin <bkashirin@outlook.com>
2020-03-26 22:13:48 -0400

    added routind navigation

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,8 +23,10 @@ export class NavComponent implements OnInit {
       }, 
       error => {
         this.alertify.error(error);
-      }
-    )
+      },
+      () => {
+        this.router.navigate(['/members']);
+      });
   }
 
   loggedIn(): boolean {
@@ -33,6 +36,7 @@ export class NavComponent implements OnInit {
   logout(): void {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 
 }
